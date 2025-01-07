f65ca4693f95ca695cf0b9a79231e72013a3ce79
telzo2000 <67196597+telzo2000@users.noreply.github.com>
2025-01-08 08:53:27 +0900

    config.h を作成

---
+++ firmware/config.h
@@ -0,0 +1,7 @@
+// Copyright 2023 m.ki (@telzo2000)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+#define VIAL_KEYBOARD_UID {0xDE, 0xE0, 0x26, 0xB1, 0x56, 0x33, 0x0E, 0xA4}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
